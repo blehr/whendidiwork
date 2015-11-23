5e41afb30d2c76beeb9b60a6079408a703b3f9b1
blehr <blehr.mail@gmail.com>
2015-11-23 05:29:28 -0500

    Configure Mongodb for heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ require('./app/config/google-passport')(passport);
 app.use(morgan('dev'));
 app.use(bodyParser.json({}));
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGOLAB_URI);//MONGO_URI
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
